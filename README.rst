bb20871e8dcdc033278f7e56d2a00caf49a877a4
Rob Spectre <rob@brooklynhacker.com>
2020-04-19 10:10:38 -0400

    First draft for 18 April show.

--- README.rst
+++ README.rst
@@ -1,8 +1,9 @@
 ***************
-childsafe.ai Reveal Template
+Cameo Price Is Right
 ***************
 
-A template project for `Reveal.js`_ presentations used for my `childsafe.ai`_ talks.
+A game show for the cost conscious pop culture celebrity wonk delivered over
+video conference.
 
 
 **Table of Contents**
@@ -14,83 +15,7 @@ A template project for `Reveal.js`_ presentations used for my `childsafe.ai`_ ta
     :backlinks: none
 
 
-Features
-============
-
-* Photo cards
-* Headline cards
-* Charts with `C3`_
-* Map cards with `Google Maps`_
-* Funnel and Pyramid charts with `Highcharts`_
-* Network visualizations with `Viz.js`_
-* YouTube embeds
-
-
-Installation
-============
-
-Clone repo
-
-.. code-block:: bash
-
-    $ git clone https://github.com/childsafeai/Reveal-Template.git 
-
-
-Install ``gulp`` and ``bower``
-
-.. code-block:: bash
-    
-    $ npm install -g bower gulp
-
-Install node dependencies
-
-.. code-block:: bash
-
-    $ npm install
-
-Install client dependencies
-
-.. code-block:: bash
-
-    $ bower install
-
-Serve presentation 
-
-.. code-block:: bash
-    
-    $ gulp serve
-
-
-Deploy to Heroku
-================
-
-This template comes with some helpers to make it easy to deploy your
-presentations to Heroku.  Once you have cloned the repo and built your
-presentation, use the following steps to deploy the presentation to Heroku.
-
-Create Heroku app.
-
-.. code-block:: bash
-
-    $ heroku create
-
-Configure Heroku app to use both Node and Python buildpacks. 
-.. code-block:: bash
-
-    $ heroku buildpacks:set heroku/nodejs
-    $ heroku buildpacks:add --index 1 heroku/python
-
-Push your presentation to Heroku
-
-.. code-block:: bash
-
-    $ git push heroku master
-
-Open the presentation on your newly deployed host.
-
-.. code-block:: bash
-
-    $ heroku open
+Not ready for primetime yet - still iterating.
 
 
 Meta
@@ -100,11 +25,5 @@ Meta
 * Released under `MIT License`_
 * Software is as is - no warranty expressed or implied.
 
-.. _childsafe.ai: https://childsafe.ai
-.. _Reveal.js: http://lab.hakim.se/reveal-js/
-.. _C3: http://c3js.org/
-.. _Google Maps: https://developers.google.com/maps/documentation/javascript/
-.. _Highcharts: http://www.highcharts.com/
-.. _Viz.js: http://visjs.org/
 .. _Rob Spectre: http://www.brooklynhacker.com
 .. _MIT License: http://opensource.org/licenses/MIT
--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,11 @@
 function processCustomSlide(currentSlide) {
-  // Slide level interactions go here.
+  slide = currentSlide;
+  chart_div = slide.getElementsByTagName('div')[0];
+  id = chart_div.getAttribute('id');
+
+  if ($(chart_div).hasClass("results")) {
+    createResultsChart(chart_div);
+  }
 }
 
 function advanceCustom(event) {
@@ -32,18 +38,48 @@ function players() {
       this.players = this.players.filter(player => name !== player.name);
       storeObject('players', this.players);
     },
-    addGuess(player_name) {
-      if (this.guess.trim() === '') {
+    addGuess(player_name, guess) {
+      if (guess.trim() === '') {
         return;
       }
 
+      new_state = [];
+
       this.players.forEach(function(player) {
         if (player.name == player_name) {
-          this.players[player_name].guess = this.guess;
+          player.guess = parseInt(guess);
+        }
+        new_state.push(player);
+      });
+
+      this.players = new_state;
+
+      storeObject('players', new_state);
+    },
+    findWinner(correct_answer) {
+      var best_guess = 0;
+      var winner = "";
+
+      this.players.forEach(function(player) {
+        if (player.guess > best_guess && player.guess <= correct_answer) {
+          best_guess = player.guess;
+          winner = player.name;
         }
       });
 
-      storeObject('players', this.players);
+      new_state = [];
+
+      this.players.forEach(function(player) {
+        if (player.name == winner) {
+          player.score++;
+        }
+        new_state.push(player);
+      });
+
+      this.players = new_state;
+
+      storeObject('players', new_state);
+      alert("Correct answer is " + correct_answer + ". The winner is " + winner + " with " + best_guess + "!");
     }
   }
 }
@@ -56,6 +92,62 @@ function getObject(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
+function createResultsChart(div) {
+  var players = getObject('players');
+
+  var data = {
+    type: 'bar',
+    axis: {
+      rotated: true
+    },
+  }
+
+  var x = [];
+  var values = ['Score']
+
+  players.forEach(function(player) {
+    x.push(player.name);
+    values.push(player.score);
+  });
+
+  data.columns = [values];
+
+  var options = {
+    bindto: div, 
+    data: data,
+    axis: {
+      rotated: true,
+      x: {
+        type: 'category',
+        categories: x
+      }
+    }
+  };
+
+  console.log(options);
+
+  console.log(data.columns);
+ 
+  return c3.generate(options);
+}
+
+
 if (localStorage.getItem('players') === null) {
   localStorage.setItem('players', "[]");
+}
+
+function setPlayerScore(player_name, score) {
+  var players = getObject('players');
+
+  new_state = [];
+
+  players.forEach(function(player) {
+    if (player.name == player_name) {
+      player.score = score;
+    }
+
+    new_state.push(player);
+  });
+
+  storeObject('players', new_state);
 }
